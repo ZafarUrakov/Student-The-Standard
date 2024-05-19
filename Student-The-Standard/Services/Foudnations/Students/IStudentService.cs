b12ff1821610b1a22d79132da3ce81f71f23f45a
ZafarUrakov <goliburakov535@gmail.com>
2024-05-19 21:01:05 +0500

    FOUNDATIONS: Student Add

--- Student-The-Standard/Services/Foudnations/Students/IStudentService.cs
+++ Student-The-Standard/Services/Foudnations/Students/IStudentService.cs
@@ -1,6 +1,10 @@
-﻿namespace Student_The_Standard.Services.Foudnations.Students
+﻿using Student_The_Standard.Models.Students;
+using System.Threading.Tasks;
+
+namespace Student_The_Standard.Services.Foudnations.Students
 {
     public interface IStudentService
     {
+        ValueTask<Student> AddStudentAsync(Student student);
     }
 }
